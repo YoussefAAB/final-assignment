8f7e11293b0ccfcacc6dcaeea044933d5f0b4af0
YoussefAAB <168686072+YoussefAAB@users.noreply.github.com>
2024-05-01 23:12:12 +0100

    Add files via upload

---
+++ final assignment.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## final assignment
+
+# Welcome to my final project notebook. In this notebook, we will explore various aspects of data science, including languages, libraries, tools, and basic arithmetic expressions.
+
+# In data science, various programming languages are used to analyze and manipulate data. Some of them are:
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. Scala
+# 6. Java
+
+# In data science, various libraries are available in different programming languages to facilitate data analysis and visualization and machine learning . Some of them:
+# 
+# - **Python**:
+#   1. NumPy
+#   2. Pandas
+#   3. Matplotlib
+#   4. Seaborn
+#   5. Scikit-learn
+#   6. TensorFlow
+#   7. PyTorch
+# 
+# - **R**:
+#   1. dplyr
+#   2. ggplot
+#   3. caret
+#   
+# 
+# 
+# 
+
+# Data Science Tools
+# 
+# | Category          | Tools                           |
+# |-------------------|---------------------------------|
+# | Data Manipulation | Pandas (Python), dplyr (R)      |
+# | Data Visualization| Matplotlib (Python), ggplot (R)|
+# | Machine Learning  | Scikit-learn (Python), caret (R)|
+# | Deep Learning     | TensorFlow, Keras, PyTorch (Python) |
+# 
+# 
+
+# In this section, we'll explore various arithmetic expressions to demonstrate basic mathematical operations such as:
+# Addition, subtraction, multiplication, and division. 
+# 
+# After we can find examples of addition and multiplication.
+# 
+
+# In[3]:
+
+
+x=5
+y=6
+
+add=y+x
+
+multi=y*x
+
+print("Addition result:", add)
+print("Multiplication result:", multi)
+
+
+# In[6]:
+
+
+minutes = 240
+
+hours = minutes / 60
+
+print(hours, "hours")
+
+
+# The main objectives of this project is:
+# 
+# 1. Understanding basic data science concepts.
+# 2. Exploring various data science languages, libraries, and tools.
+# 3. Practicing arithmetic expressions and basic operations in programming languages.
+# 4. Sharing the final project through GitHub .
+# 5. Gaining experience in creating and presenting structured documents using Jupyter Notebook.
+# 
+
+# Youssef AABBAD
+
+# In[ ]:
+
+
+
+
